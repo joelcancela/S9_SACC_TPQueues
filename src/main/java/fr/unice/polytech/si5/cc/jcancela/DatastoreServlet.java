b0de72d8972247416ee8a445e96eb5cb2c659c40
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2018-11-01 21:27:10 +0100

    Exo Entités et requêtes simples

--- src/main/java/fr/unice/polytech/si5/cc/jcancela/DatastoreServlet.java
+++ src/main/java/fr/unice/polytech/si5/cc/jcancela/DatastoreServlet.java
@@ -1,57 +1,61 @@
 package fr.unice.polytech.si5.cc.jcancela;
 
-import com.google.cloud.Timestamp;
+import com.google.appengine.repackaged.com.google.gson.Gson;
 import com.google.cloud.datastore.*;
+import fr.unice.polytech.si5.cc.jcancela.model.Article;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.Inet4Address;
-import java.net.Inet6Address;
-import java.net.InetAddress;
 
 @SuppressWarnings("serial")
 @WebServlet
 public class DatastoreServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,
-          ServletException {
-    // store only the first two octets of a users ip address
-    String userIp = req.getRemoteAddr();
-    InetAddress address = InetAddress.getByName(userIp);
-    if (address instanceof Inet6Address) {
-      // nest indexOf calls to find the second occurrence of a character in a string
-      // an alternative is to use Apache Commons Lang: StringUtils.ordinalIndexOf()
-      userIp = userIp.substring(0, userIp.indexOf(":", userIp.indexOf(":") + 1)) + ":*:*:*:*:*:*";
-    } else if (address instanceof Inet4Address) {
-      userIp = userIp.substring(0, userIp.indexOf(".", userIp.indexOf(".") + 1)) + ".*.*";
-    }
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,
+			ServletException {
+		Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+		Query<Entity> query = Query.newEntityQueryBuilder().setKind("article").build();
+		QueryResults<Entity> results = datastore.run(query);
+		resp.setContentType("text/plain");
+		PrintWriter out = resp.getWriter();
+		while (results.hasNext()) {
+			Entity entity = results.next();
+			out.format("Article: %s Prix: %s Qte: %s \n", entity.getString("nom"), entity.getDouble("prix"),
+					entity.getLong("qte"));//Int => Long
+		}
+	}
 
-    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-    KeyFactory keyFactory = datastore.newKeyFactory().setKind("visit");
-    IncompleteKey key = keyFactory.setKind("visit").newKey();
-
-    // Record a visit to the datastore, storing the IP and timestamp.
-    FullEntity<IncompleteKey> curVisit = FullEntity.newBuilder(key)
-        .set("user_ip", userIp).set("timestamp", Timestamp.now()).build();
-    datastore.add(curVisit);
-
-    // Retrieve the last 10 visits from the datastore, ordered by timestamp.
-    Query<Entity> query = Query.newEntityQueryBuilder().setKind("visit")
-        .setOrderBy(StructuredQuery.OrderBy.desc("timestamp")).setLimit(10).build();
-    QueryResults<Entity> results = datastore.run(query);
-    resp.setContentType("text/plain");
-    PrintWriter out = resp.getWriter();
-    out.print("Last 10 visits:\n");
-    while (results.hasNext()) {
-      Entity entity = results.next();
-      out.format("Time: %s Addr: %s\n", entity.getTimestamp("timestamp"),
-          entity.getString("user_ip"));
-    }
-  }
+	//Ex to test in Postman: {"nom":"Pomme","prix":7.8,"qte":77}
+	//Ex to test in Postman: {"nom":"Poire","prix":7.7,"qte":77}
+	//Delete entity with ?delete= <name>
+	//Delete: http://localhost:8080/datastore?delete=Pomme
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+		if(req.getParameterMap().size()>0 && req.getParameterMap().containsKey("delete")){
+			StructuredQuery.PropertyFilter nameFilter = StructuredQuery.PropertyFilter.eq("nom",req.getParameter(
+					"delete"));
+			Query<Entity> query = Query.newEntityQueryBuilder().setKind("article").setFilter(nameFilter).build();
+			QueryResults<Entity> results = datastore.run(query);
+			while (results.hasNext()) {
+				Entity entity = results.next();
+				datastore.delete(entity.getKey());
+			}
+		}else{
+			KeyFactory keyFactory = datastore.newKeyFactory().setKind("article");
+			BufferedReader reader = req.getReader();
+			Article articleInput = new Gson().fromJson(reader, Article.class);
+			IncompleteKey key = keyFactory.setKind("article").newKey();
+			FullEntity<IncompleteKey> aNewArticle = FullEntity.newBuilder(key)
+					.set("nom", articleInput.getNom()).set("prix", articleInput.getPrix()).set("qte", articleInput.getQte()).build();
+			datastore.add(aNewArticle);
+		}
+	}
 }
